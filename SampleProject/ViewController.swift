a66c23ec803042e18fffc42e57cb7bda51c06d6a
Sao Saechao <ssaechao@gmail.com>
2016-10-22 17:06:18 -0700

    Initial Commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Sao Saechao on 10/22/16.
+//  Copyright © 2016 tAppCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
